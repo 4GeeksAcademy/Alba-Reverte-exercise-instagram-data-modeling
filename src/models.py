0b46ff840434d2fe406e96cf2aef89a593cf0f7b
AlbaReverte <albareverteruiz@gmail.com>
2024-12-11 13:15:57 +0000

    sql instagram

--- src/models.py
+++ src/models.py
@@ -7,23 +7,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(50), nullable=False)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    comment = relationship ('comment', backref="user")
+    post = relationship ('post', backref="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    date = Column(Integer)
+    description = Column(String(250))
+    user_id = Column(Integer, ForeignKey ("user.id"))
+    media_id = Column(Integer, ForeignKey ("media.id"))
+    comment = relationship ('comment', backref="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    
+    id = Column(Integer, primary_key=True)
+    created_at = Column(Integer)
+    comment = Column(String(250))
+    user_id = Column(Integer, ForeignKey ("user.id"))
+    post_id = Column(Integer, ForeignKey ("post.id"))
+
+class Media(Base):
+    __tablename__ = 'media'
+    
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250))
+    type = Column(enumerate(MediaType))
+    post = relationship ('post', backref="media")
+    
 
     def to_dict(self):
         return {}
